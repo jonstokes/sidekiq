cecec2327015a0d10ffbed1af27d134c5d3b77a5
Jon Stokes <jon@paperhatpress.com>
2013-06-04 19:41:52 -0700

    Revert to using task threads instead of task fibers

--- lib/sidekiq/processor.rb
+++ lib/sidekiq/processor.rb
@@ -1,5 +1,5 @@
+require 'celluloid'
 require 'sidekiq/util'
-require 'sidekiq/actor'
 
 require 'sidekiq/middleware/server/active_record'
 require 'sidekiq/middleware/server/retry_jobs'
@@ -11,12 +11,8 @@ module Sidekiq
   # processes it.  It instantiates the worker, runs the middleware
   # chain and then calls Sidekiq::Worker#perform.
   class Processor
-    STATS_TIMEOUT = 180 * 24 * 60 * 60
-
     include Util
-    include Actor
-
-    task_class TaskThread
+    include Celluloid
 
     def self.default_middleware
       Middleware::Chain.new do |m|
@@ -38,29 +34,29 @@ module Sidekiq
     def process(work)
       msgstr = work.message
       queue = work.queue_name
+      defer do
+        @actual_work_thread = Thread.current
+        begin
+          msg = Sidekiq.load_json(msgstr)
+          klass  = msg['class'].constantize
+          worker = klass.new
+          worker.jid = msg['jid']
 
-      @actual_work_thread = Thread.current
-      begin
-        msg = Sidekiq.load_json(msgstr)
-        klass  = msg['class'].constantize
-        worker = klass.new
-        worker.jid = msg['jid']
-
-        stats(worker, msg, queue) do
-          Sidekiq.server_middleware.invoke(worker, msg, queue) do
-            worker.perform(*cloned(msg['args']))
+          stats(worker, msg, queue) do
+            Sidekiq.server_middleware.invoke(worker, msg, queue) do
+              worker.perform(*cloned(msg['args']))
+            end
           end
+        rescue Sidekiq::Shutdown
+          # Had to force kill this job because it didn't finish
+          # within the timeout.
+        rescue Exception => ex
+          handle_exception(ex, msg || { :message => msgstr })
+          raise
+        ensure
+          work.acknowledge
         end
-      rescue Sidekiq::Shutdown
-        # Had to force kill this job because it didn't finish
-        # within the timeout.
-      rescue Exception => ex
-        handle_exception(ex, msg || { :message => msgstr })
-        raise
-      ensure
-        work.acknowledge
       end
-
       @boss.async.processor_done(current_actor)
     end
 
@@ -89,25 +85,21 @@ module Sidekiq
         yield
       rescue Exception
         redis do |conn|
-          failed = "stat:failed:#{Time.now.utc.to_date}"
-          result = conn.multi do
+          conn.multi do
             conn.incrby("stat:failed", 1)
-            conn.incrby(failed, 1)
+            conn.incrby("stat:failed:#{Time.now.utc.to_date}", 1)
           end
-          conn.expire(failed, STATS_TIMEOUT) if result.last == 1
         end
         raise
       ensure
         redis do |conn|
-          processed = "stat:processed:#{Time.now.utc.to_date}"
-          result = conn.multi do
+          conn.multi do
             conn.srem("workers", identity)
             conn.del("worker:#{identity}")
             conn.del("worker:#{identity}:started")
             conn.incrby("stat:processed", 1)
-            conn.incrby(processed, 1)
+            conn.incrby("stat:processed:#{Time.now.utc.to_date}", 1)
           end
-          conn.expire(processed, STATS_TIMEOUT) if result.last == 1
         end
       end
     end
